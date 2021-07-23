c39de0ff39927ccc6513d746fc9ef47a3d38a30d
Jakub <mattrusz@gmail.com>
2021-07-23 21:16:07 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mac-spoofer
 MAC spooffer for macOS®
+
+Just run it wherever, type password in, and enjoy having your MAC address changed
---
+++ src/spoofer.sh
@@ -0,0 +1,12 @@
+main()
+{
+        SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')
+        RANDOMISED_MAC=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
+        INTERFACE=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}')
+        echo $SSID $RANDOMISED_MAC $INTERFACE
+        sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport "$INTERFACE" -z
+        sudo ifconfig $INTERFACE ether $RANDOMISED_MAC
+        networksetup -setairportnetwork $INTERFACE $SSID
+}
+main
+
