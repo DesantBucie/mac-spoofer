b5b97f220f572799a81a9e15290bc2a87cbd77d8
Jakub <mattrusz@gmail.com>
2021-09-22 20:57:59 +0200

    Update spoofer.sh

--- src/spoofer.sh
+++ src/spoofer.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env sh
 main()
 {
         SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')
         RANDOMISED_MAC=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
         INTERFACE=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}')
-        echo $SSID $RANDOMISED_MAC $INTERFACE
+        echo "$SSID" "$RANDOMISED_MAC" "$INTERFACE"
         sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport "$INTERFACE" -z
-        sudo ifconfig $INTERFACE ether $RANDOMISED_MAC
-        networksetup -setairportnetwork $INTERFACE $SSID
+        sudo ifconfig $INTERFACE ether "$RANDOMISED_MAC"
+        networksetup -setairportnetwork "$INTERFACE" "$SSID"
 }
 main
 
