0b74b11ccdb02a5d138e420070d68744775e1cf8
DesantBucie <mattrusz@gmail.com>
2021-10-02 16:18:53 +0200

    Added two new modes

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # mac-spoofer
 
-Just run spoofer.sh wherever, type password in, and enjoy having your MAC address changed
+Run it once with no arguments and have you mac address changed
+
+To use arguments you need either sudo or doas set up that way, so they do not need passwords
+
+then you have to options
+-c = continouous
+-s = sequences
+
+`./spoofer.sh -c <number of minutes between change>`
+
+`./spoofer.sh -s <numver of minutes between change> <number of sequences>`
--- src/spoofer.sh
+++ src/spoofer.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env sh
+
 main()
 {
         SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')
         RANDOMISED_MAC=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
         INTERFACE=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}')
         echo "$SSID" "$RANDOMISED_MAC" "$INTERFACE"
-        sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport "$INTERFACE" -z
-        sudo ifconfig $INTERFACE ether "$RANDOMISED_MAC"
+        $SUDO /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport "$INTERFACE" -z
+        $SUDO ifconfig "$INTERFACE" ether "$RANDOMISED_MAC"
         networksetup -setairportnetwork "$INTERFACE" "$SSID"
 }
-main
+if [ "$(command -v doas)" ]; then
+    SUDO=doas
+else 
+    SUDO=sudo
+fi
+if [ "$1" = "-c" ]; then
+    while true; do
+        main
+        sleep $(( 60*$2 ))
+    done
+elif [ "$1" = "-s" ]; then
+    n=0
+    while [ "$n" -le "$3" ]; do
+        main
+        sleep $(( 60*$2 ))
+        n=$(( n+1 ))
+    done
+fi
 
